1f8299094e07edf53fd629fb6d1a9110c07ecf31
llxchen <sweet_chenqiang@163.com>
2020-09-08 23:55:04 +0800

    移动零

---
+++ Week_01/283.移动零.java
@@ -0,0 +1,24 @@
+import jdk.nashorn.internal.ir.ReturnNode;
+
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length;i++) {
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
+// @lc code=end
+
